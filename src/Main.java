410c579d50100bdde04ed2c7752946bac5d6924b
karishka1222 <karinarej9@gmail.com>
2024-10-02 21:31:39 +0300

    Added: code structure(class Matrix, class Vector, input)

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // input
+        Scanner scanner = new Scanner(System.in);
+        int functionSize = scanner.nextInt();
+        int[] coeffFunction = new int[functionSize];
+        for (int i = 0; i < functionSize; i++) {
+            coeffFunction[i] = scanner.nextInt();
+        }
+        int rowConstraintSize = scanner.nextInt();
+        int columnConstraintSize = scanner.nextInt();
+        int[][] coeffConstraints = new int[rowConstraintSize][columnConstraintSize];
+        for (int i = 0; i < rowConstraintSize; i++) {
+            for (int j = 0; j < columnConstraintSize; j++) {
+                coeffConstraints[i][j] = scanner.nextInt();
+            }
+        }
+        int rightHandSize = scanner.nextInt();
+        int[] rightHandValues = new int[rightHandSize];
+        for (int i = 0; i < rightHandSize; i++) {
+            rightHandValues[i] = scanner.nextInt();
+        }
+        int accuracy = scanner.nextInt();
+
+        // creating elements
+        Vector function = new Vector(functionSize, coeffFunction);
+        Matrix constraints = new Matrix(rowConstraintSize, columnConstraintSize, coeffConstraints);
+        Vector rightConstraints = new Vector(rightHandSize, rightHandValues);
+
+        // output results
+        if (true) {
+            System.out.println("The method is not applicable!");
+        } else {
+
+        }
+    }
+
+    public static void simplexMethod(Vector C, Matrix A, Vector b, int accuracy) {
+
+    }
+}
+
+
+class Vector {
+    private int n;
+    private int[] values;
+    Vector(int n, int[] values) {
+        this.n = n;
+        this.values = values;
+    }
+}
+
+class Matrix {
+    private int n;
+    private int m;
+    private int[][] values;
+    Matrix(int n, int m, int[][] values) {
+        this.n = n;
+        this.m = m;
+        this.values = values;
+    }
+}
